a8a070cd126e900b856085930cc930f718615890
FilthT <trislan123@hotmail.com>
2022-04-15 19:23:47 -0700

    Update MarkdownParse.java
    fixed the problem when the file has no links

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,7 +11,10 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        if (!markdown.contains("[") && !markdown.contains("]")) {
+            return toReturn;
+        }
+        while (currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
@@ -19,15 +22,13 @@ public class MarkdownParse {
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
-
         return toReturn;
     }
-
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
     }
 }
