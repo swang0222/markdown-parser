3fd28c272cdea62985a9259453810ff5aabeba22
FilthT <trislan123@hotmail.com>
2022-04-22 18:43:53 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -22,4 +22,20 @@ public class MarkdownParseTest {
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(String, list);
     }
+
+    @Test
+    public void getLinks1() throws IOException {
+        List<String> String = List.of("https://something.com", "some-thing.html");
+        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file.md");
+        ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
+        assertEquals(String, list);
+    }
+
+    @Test
+    public void getLinks2() throws IOException {
+        ArrayList<String> empty = new ArrayList<String>();
+        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-failed-2.md");
+        ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
+        assertEquals(empty, list);
+    }
 }
