4b7068579348f41b9b2e90e26ae4492d5400b142
FilthT <trislan123@hotmail.com>
2022-04-22 18:55:03 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -32,10 +32,66 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void getLinks2() throws IOException {
+    public void getLinks0() throws IOException {
         ArrayList<String> empty = new ArrayList<String>();
         Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-failed-2.md");
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(empty, list);
+    }
+
+    @Test
+    public void getLinks2() throws IOException {
+        List<String> String = List.of("https://something.com", "some-thing.html");
+        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file2.md");
+        ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
+        assertEquals(String, list);
+    }
+
+    @Test
+    public void getLinks3() throws IOException {
+        List<String> String = List.of("https://something.com", "some-thing.html");
+        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file3.md");
+        ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
+        assertEquals(String, list);
+    }
+
+    @Test
+    public void getLinks4() throws IOException {
+        List<String> String = List.of("https://something.com", "some-thing.html");
+        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file4.md");
+        ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
+        assertEquals(String, list);
+    }
+
+    @Test
+    public void getLinks5() throws IOException {
+        List<String> String = List.of("https://something.com", "some-thing.html");
+        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file5.md");
+        ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
+        assertEquals(String, list);
+    }
+
+    @Test
+    public void getLinks6() throws IOException {
+        List<String> String = List.of("https://something.com", "some-thing.html");
+        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file6.md");
+        ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
+        assertEquals(String, list);
+    }
+
+    @Test
+    public void getLinks7() throws IOException {
+        List<String> String = List.of("https://something.com", "some-thing.html");
+        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file7.md");
+        ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
+        assertEquals(String, list);
+    }
+
+    @Test
+    public void getLinks8() throws IOException {
+        List<String> String = List.of("https://something.com", "some-thing.html");
+        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file8.md");
+        ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
+        assertEquals(String, list);
     }
 }
