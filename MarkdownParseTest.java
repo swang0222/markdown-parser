e047800715e72586f15ec51eb93a33d059f57acd
FilthT <trislan123@hotmail.com>
2022-04-29 19:08:02 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -26,7 +26,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks1() throws IOException {
         List<String> String = List.of("https://something.com", "some-thing.html");
-        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file.md");
+        Path file = Path.of("test-file.md");
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(String, list);
     }
@@ -34,7 +34,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks2() throws IOException {
         List<String> String = List.of("https://something.com", "some-page.html");
-        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file2.md");
+        Path file = Path.of("test-file2.md");
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(String, list);
     }
@@ -42,7 +42,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks3() throws IOException {
         ArrayList<String> empty = new ArrayList<String>();
-        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file3.md");
+        Path file = Path.of("test-file3.md");
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(empty, list);
     }
@@ -50,7 +50,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks4() throws IOException {
         ArrayList<String> empty = new ArrayList<String>();
-        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file4.md");
+        Path file = Path.of("test-file4.md");
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(empty, list);
     }
@@ -58,7 +58,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks5() throws IOException {
         ArrayList<String> empty = new ArrayList<String>();
-        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file5.md");
+        Path file = Path.of("test-file5.md");
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(empty, list);
     }
@@ -66,7 +66,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks6() throws IOException {
         ArrayList<String> empty = new ArrayList<String>();
-        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file6.md");
+        Path file = Path.of("test-file6.md");
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(empty, list);
     }
@@ -74,7 +74,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks7() throws IOException {
         ArrayList<String> empty = new ArrayList<String>();
-        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file7.md");
+        Path file = Path.of("test-file7.md");
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(empty, list);
     }
@@ -82,7 +82,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks8() throws IOException {
         ArrayList<String> empty = new ArrayList<String>();
-        Path file = Path.of("C:/Users/trisl/OneDrive/Documents/GitHub/markdown-parser/test-file8.md");
+        Path file = Path.of("test-file8.md");
         ArrayList<String> list = MarkdownParse.getLinks(Files.readString(file));
         assertEquals(empty, list);
     }
